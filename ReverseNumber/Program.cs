f54fccb3057bc8325b11d2a7a2225bf37a65ea56
Mjchaurasia9 <92732297+Mjchaurasia9@users.noreply.github.com>
2022-04-27 01:21:28 +0530

    Add files via upload

---
+++ ReverseNumber/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num, reverse = 0;
+Console.Write("Enter a Number to Reverse:\t");
+num = int.Parse(Console.ReadLine());
+while(num != 0)
+{
+reverse = reverse * 10;
+reverse = reverse + num % 10;
+num = num / 10;
+}
+Console.WriteLine($"Reverse Number is {reverse}");
